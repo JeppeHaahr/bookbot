77b67062e7adc723450b9f8c7cf1b40e63afad14
jeppeschartau <jeppeschartau@gmail.com>
2025-08-13 11:10:08 +0200

    Finished!

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from stats import get_book_word_count
 from stats import get_characters
 from stats import sort_list
+import sys
 
 def main():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {get_book_word_count("books/frankenstein.txt")} total words")
-    print("--------- Character Count -------")
-    for chara in sort_list(get_characters("books/frankenstein.txt")):
-        print(f"{chara["char"]}: {chara["num"]}")
-    #print(sort_list(get_characters("books/frankenstein.txt")))
+    if len(sys.argv) == 2:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {get_book_word_count(sys.argv[1])} total words")
+        print("--------- Character Count -------")
+        for chara in sort_list(get_characters(sys.argv[1])):
+            print(f"{chara["char"]}: {chara["num"]}")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
